3e08780301eb35e8605c4df450fb3a92085a4218
aditya bintang fadila <adityabintang149@gmail.com>
2025-12-22 13:13:01 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -62,6 +62,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS when behind proxy (Easypanel, Cloudflare, etc.)
+        if ($this->app->environment('production') || str_starts_with(config('app.url'), 'https')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
