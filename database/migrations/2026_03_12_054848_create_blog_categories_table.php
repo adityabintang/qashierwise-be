cfab3e94178184e686db1502271d8b672ec1deba
itsanla <anlaharpanda@gmail.com>
2026-03-12 12:49:28 +0700

    feat: Add columns to blog_categories table for name, slug, description, and SEO fields

--- database/migrations/2026_03_12_054848_create_blog_categories_table.php
+++ database/migrations/2026_03_12_054848_create_blog_categories_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->string('seo_title')->nullable();
+            $table->text('seo_description')->nullable();
             $table->timestamps();
         });
     }
