7a16769b74da6a6f101efc06f0e75e649c1e866e
itsanla <anlaharpanda@gmail.com>
2026-03-12 13:04:26 +0700

    feat: Update import statements for Section component in BlogCategoryForm and BlogPostForm; enhance BlogCategoryResourceTest and BlogTagResourceTest with comprehensive CRUD operations and validations

--- app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
+++ app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
@@ -9,7 +9,7 @@ use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
