f97d8a575410c0f58809c16d3aa9b11aad5f28e3
itsanla <anlaharpanda@gmail.com>
2026-04-01 08:39:35 +0700

    test: add additional tests for timezone formatting with cookie timezones

--- tests/Unit/TimezoneDisplayHelperTest.php
+++ tests/Unit/TimezoneDisplayHelperTest.php
@@ -42,4 +42,30 @@ class TimezoneDisplayHelperTest extends TestCase
 
         $this->assertSame('25 Mar 2026 10:39 WITA', $formatted);
     }
+
+    public function test_it_uses_cookie_timezone_for_indonesia_wit(): void
+    {
+        app()->setLocale('en');
+
+        request()->cookies->set('viewer_timezone', 'Asia/Jayapura');
+
+        $formatted = TimezoneDisplayHelper::formatWithLabel(
+            Carbon::parse('2026-03-25 02:39:00', 'UTC')
+        );
+
+        $this->assertSame('25 Mar 2026 11:39 WIT', $formatted);
+    }
+
+    public function test_it_falls_back_to_utc_label_for_non_indonesia_cookie_timezone(): void
+    {
+        app()->setLocale('en');
+
+        request()->cookies->set('viewer_timezone', 'Europe/Berlin');
+
+        $formatted = TimezoneDisplayHelper::formatWithLabel(
+            Carbon::parse('2026-03-25 02:39:00', 'UTC')
+        );
+
+        $this->assertSame('25 Mar 2026 02:39 UTC', $formatted);
+    }
 }
