bf74599e7db5458a87a4d11a9eeb3cead71580b7
itsanla <anlaharpanda@gmail.com>
2026-04-22 10:23:58 +0700

    fix: pyament method tidak perlu di definisikan teryata

--- app/Services/XenditSubscriptionService.php
+++ app/Services/XenditSubscriptionService.php
@@ -216,7 +216,6 @@ class XenditSubscriptionService
                 'recurring_action' => 'PAYMENT',
                 'currency' => $plan['currency'] ?? 'IDR',
                 'amount' => (int) $durationDetails['price'],
-                'payment_method_types' => ['CARD', 'DIRECT_DEBIT'],
                 'schedule' => [
                     'reference_id' => $scheduleReferenceId,
                     'interval' => 'MONTH',
