e336861b5eebaaaab93fef7c9f723e15b4afc724
itsanla <anlaharpanda@gmail.com>
2026-06-05 08:05:55 +0700

    fix: form

--- resources/views/reservation/form.blade.php
+++ resources/views/reservation/form.blade.php
@@ -469,6 +469,7 @@
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json',
+                                'Accept': 'application/json',
                                 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
                             },
                             body: JSON.stringify(this.formData)
